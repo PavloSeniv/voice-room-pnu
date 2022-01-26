e0dffdf924b11e3402f9dde038a0bd91b4171e85
PavloSeniv <senivpavlo016@gmail.com>
2022-01-26 14:00:00 +0200

    Add nprogress and remarked react welcome step

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/nprogress.css";
+import "../styles/globals.css";
+
+import Router from "next/router";
+import nProgress from "nprogress";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
