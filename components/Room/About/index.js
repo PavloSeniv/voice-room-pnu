d574738004e69db06c068f69ff24c6c3d36be12e
PavloSeniv <senivpavlo016@gmail.com>
2022-01-30 20:43:05 +0200

    Fix vercel build

--- components/Room/About/index.js
+++ components/Room/About/index.js
@@ -60,7 +60,7 @@ export default function Room(params) {
                 viewbox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
                 />
               </svg>
@@ -77,7 +77,7 @@ export default function Room(params) {
               <div className={styles.about__user_info_main}>
                 <picture>
                   <source
-                    srcset="img/index/main/avatar_placeholder.webp"
+                    srcSet="img/index/main/avatar_placeholder.webp"
                     type="image/webp"
                   />
                   <Image
@@ -95,7 +95,7 @@ export default function Room(params) {
                 </div>
 
                 <button
-                  onclick=""
+                  onClick=""
                   type="submit"
                   className={styles.about__user_button_follow}
                 >
@@ -112,8 +112,8 @@ export default function Room(params) {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
                         d="M36 19.2C36 20.9673 34.6569 22.4 33 22.4C31.3431 22.4 30 20.9673 30 19.2C30 17.4327 31.3431 16 33 16C34.6569 16 36 17.4327 36 19.2ZM36 32C36 33.7673 34.6569 35.2 33 35.2C31.3431 35.2 30 33.7673 30 32C30 30.2327 31.3431 28.8 33 28.8C34.6569 28.8 36 30.2327 36 32ZM33 48C34.6569 48 36 46.5673 36 44.8C36 43.0327 34.6569 41.6 33 41.6C31.3431 41.6 30 43.0327 30 44.8C30 46.5673 31.3431 48 33 48Z"
                         fill="black"
                       />
