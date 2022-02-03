c681adb0a5ea8e1ac8450dceb07b7c8d0d158de6
PavloSeniv <senivpavlo016@gmail.com>
2022-02-03 13:20:20 +0200

    Fix Name and add context and valid

--- components/loginStep/StepName/index.tsx
+++ components/loginStep/StepName/index.tsx
@@ -3,8 +3,23 @@ import Link from "next/link";
 import Button from "../../Button";
 import MainBlock from "../../MainBlock";
 import Image from "next/image";
+import React from "react";
+import { MainContext } from "../../../pages";
 
 export default function Name(params) {
+  const [inputValue, setInputValue] = React.useState<string>("");
+  const { onNextStep } = React.useContext(MainContext);
+
+  const nextDisabled = !inputValue;
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onClickNextStep = () => {
+    onNextStep();
+  };
+
   return (
     <MainBlock>
       <h1 className={styles.main__title}>What`s yor full name?</h1>
@@ -16,10 +31,16 @@ export default function Name(params) {
           className={styles.main__inputName}
           type="text"
           placeholder="Pavlo Seniv"
+          value={inputValue}
+          onChange={handleChangeInput}
         />
       </form>
 
-      <Button>
+      <Button
+        disabled={nextDisabled}
+        onClick={onClickNextStep}
+        className={styles.main__button}
+      >
         <h3 className={styles.main__buttonTitle}>Next</h3>
         <Image src="/static/svgicons/arrow-right.svg" height={20} width={21} />
       </Button>
