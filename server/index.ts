a46a98d65cf50645ed0de30845e2c0f899f06481
PavloSeniv <senivpavlo016@gmail.com>
2022-02-16 23:04:24 +0200

    Add send frontend from backend avatar. Change server https on the http

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import fs from "fs";
 import https from "https";
+import http from "http";
 import dotenv from "dotenv";
+import multer from "multer";
+import cors from "cors";
+import { nanoid } from "nanoid";
 
 // Cerf
-const httpsOptions = {
-  key: fs.readFileSync("./cerf/server-key.pem"), // путь к ключу
-  cert: fs.readFileSync("./cerf/server-cert.pem"), // путь к сертификату
-};
+// const httpsOptions = {
+//   key: fs.readFileSync("./cerf/server-key.pem"), // путь к ключу
+//   cert: fs.readFileSync("./cerf/server-cert.pem"), // путь к сертификату
+// };
 
 dotenv.config({
   path: "server/.env",
@@ -19,7 +23,31 @@ import { passport } from "./core/passport";
 
 const app = express();
 
+const uploader = multer({
+  storage: multer.diskStorage({
+    destination: function (_, __, cb) {
+      cb(null, "./public/avatars/");
+    },
+    filename: function (_, file, cb) {
+      cb(
+        null,
+        file.fieldname + "-" + nanoid(6) + "." + file.mimetype.split("/").pop()
+      );
+    },
+  }),
+});
+
+app.use(cors());
+
 app.use(passport.initialize());
+
+app.post("/upload", uploader.single("photo"), (req, res) => {
+  res.json(req.file);
+});
+
+app.get("/test", (req, res) => {
+  res.json("123");
+});
 
 app.get("/auth/github", passport.authenticate("github"));
 
@@ -39,6 +67,12 @@ app.get(
   }
 );
 
-https.createServer(httpsOptions, app).listen(3001, () => {
-  console.log("Server Runned!!!");
-});
+// https.createServer(httpsOptions, app).listen(3001, () => {
+//   console.log("Server Runned!!!");
+// });
+
+// app.listen(3001, () => {
+//   console.log("Server Runned!!!");
+// });
+
+http.createServer(app).listen(3001, () => {});
