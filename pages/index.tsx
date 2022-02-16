e9a94979535e933034c4a2cc3a01e4ca90424520
PavloSeniv <senivpavlo016@gmail.com>
2022-02-16 17:50:03 +0200

    Add type for User. Create arrow func for changes in json property user

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import DoneGoogle from "../components/steps/StepImportDone";
 import ImportGoogle from "../components/steps/StepImportGoogle";
 import React from "react";
 
-const stepsCpmponents = {
+const stepsComponents = {
   0: Welcome,
   1: ImportGoogle,
   2: Name,
@@ -18,9 +18,21 @@ const stepsCpmponents = {
   5: PhoneActivation,
 };
 
+type UserProps = {
+  id: number;
+  fullname: string;
+  username: string;
+  avatarUrl: string;
+  phone: string;
+  isActive: number;
+};
+
 type MainContextProps = {
   onNextStep: () => void;
+  setUserData: React.Dispatch<React.SetStateAction<UserProps>>;
   step: number;
+  userData: UserProps;
+  setFieldValue: (field: keyof UserProps, value: string) => void;
 };
 
 export const MainContext = React.createContext<MainContextProps>(
@@ -29,16 +41,29 @@ export const MainContext = React.createContext<MainContextProps>(
 
 export default function Login() {
   const [step, setStep] = React.useState<number>(0);
-  const Step = stepsCpmponents[step];
+  const Step = stepsComponents[step];
+
+  const [userData, setUserData] = React.useState<UserProps>();
 
   const onNextStep = () => {
     setStep((prev) => prev + 1);
   };
 
+  const setFieldValue = (field: string, value: string) => {
+    setUserData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  console.log(userData);
+
   return (
     <div className={styles.bg__login}>
       <main className={styles.main + " " + styles.main__container}>
-        <MainContext.Provider value={{ step, onNextStep }}>
+        <MainContext.Provider
+          value={{ step, onNextStep, userData, setUserData, setFieldValue }}
+        >
           <Step />
         </MainContext.Provider>
         {/* <Step onNextStep={() => setStep(step + 1)} /> */}
