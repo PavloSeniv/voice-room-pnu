3dfb7a0c0a794c30a7c57e87cacf05e80de2a220
PavloSeniv <senivpavlo016@gmail.com>
2022-03-29 22:28:46 +0300

    Add headers for token

--- core/axios.ts
+++ core/axios.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const Axios = axios.create({
   // baseURL: "https://jsonplaceholder.typicode.com/",
   baseURL: "https://localhost:3001",
   // baseURL: "https://localhost:3001",
   // withCredentials: true, // CORSE
+  headers: {
+    Authorization: "Bearer " + Cookies.get("token")
+  }
 });
 
 export { Axios };
